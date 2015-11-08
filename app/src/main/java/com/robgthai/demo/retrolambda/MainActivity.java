d829dbeda50a67ae07463b493053d71c64306b29
RobGThai <thisisrobg@gmail.com>
2015-11-09 02:38:14 +0700

    Clean up unused code.

--- app/src/main/java/com/robgthai/demo/retrolambda/MainActivity.java
+++ app/src/main/java/com/robgthai/demo/retrolambda/MainActivity.java
@@ -1,34 +1,17 @@
 package com.robgthai.demo.retrolambda;
 
-import android.os.Handler;
+import android.os.Bundle;
 import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.github.api.service.GitHubService;
-import com.github.api.service.ServiceBuilder;
 import com.github.api.service.ZenService;
 import com.robgthai.demo.retrolambda.dagger.DaggerZenComponent;
-
-import java.io.IOException;
-import java.util.concurrent.Future;
-
-import retrofit.Response;
-import rx.Observable;
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.android.schedulers.HandlerScheduler;
-import rx.functions.Action1;
-import rx.functions.Func0;
-import rx.schedulers.Schedulers;
 
 import static android.os.Process.THREAD_PRIORITY_BACKGROUND;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Handler backgroundHandler;
     private TextView txtHello;
     ZenService service;
 
@@ -40,41 +23,16 @@ public class MainActivity extends AppCompatActivity {
         service = DaggerZenComponent.create().createService();
 
         txtHello = (TextView) findViewById(R.id.txtHello);
-        setupBackgroundThread();
         callZen();
     }
 
-    static class BackgroundThread extends HandlerThread {
-        BackgroundThread() {
-            super("SchedulerSample-BackgroundThread", THREAD_PRIORITY_BACKGROUND);
-        }
-    }
-
-    private void setupBackgroundThread() {
-        BackgroundThread backgroundThread = new BackgroundThread();
-        backgroundThread.start();
-        backgroundHandler = new Handler(backgroundThread.getLooper());
-    }
-
     private void callZen() {
-        //TODO Let's inject ZenService in.
-
-//        ZenService service = new ZenService();
-        Subscription sc = service.zen()
+        service.zen()
                 .doOnError(e -> showZen(e.getLocalizedMessage()))
                 .subscribe(s -> showZen(s));
     }
 
     private void showZen(String msg) {
         txtHello.setText(msg);
-    }
-
-    private void testObservable() {
-        Observable.just("1", "2", "3", "4")
-                  .subscribeOn(HandlerScheduler.from(backgroundHandler))
-                  .observeOn(AndroidSchedulers.mainThread())
-                  .map(s -> Integer.parseInt(s))
-                  .map(i -> i / 2)
-                  .subscribe(i -> Log.d("Test", "Testing: " + i));
     }
 }
